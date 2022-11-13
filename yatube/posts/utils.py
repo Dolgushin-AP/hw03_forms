67b3e725252850ce3a72f61429a663d08f8ff070
Долгушин Антон <antodolg@gmail.com>
2022-11-13 15:29:50 +0800

    после исправлений

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -7,5 +7,5 @@ def paginate(queryset, request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return {
-            'page_obj': page_obj,
+        'page_obj': page_obj,
     }
